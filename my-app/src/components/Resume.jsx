cfb45fe3110b3c4bb4af5c6b01a0a5ab873af26c
Vasyl <mr.yatsishin@gmail.com>
2022-05-23 20:57:47 +0300

    new

--- my-app/src/components/Resume.jsx
+++ my-app/src/components/Resume.jsx
@@ -4,17 +4,18 @@ import styles from "./Resume.module.css";
 import SignUP from "./signup/SignUP";
 import Admin from "./admin/Admin";
 import ProtectedRoute from "./ProtectedRoute";
-
+import { useState } from "react";
 const Resume = () => {
+  const [isAuth, setIsAuth] = useState(false)
   return (
     <div className={styles.wrapper}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/login" element={<SignUP />} />
+          <Route element={<ProtectedRoute isAuth={isAuth} setIsAuth={setIsAuth}/>}>
             <Route path="/admin" element={<Admin />} />
           </Route>
+          <Route path="/login" element={<SignUP isAuth={isAuth} setIsAuth={setIsAuth}/>} />
         </Routes>
       </BrowserRouter>
     </div>
